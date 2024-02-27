93f24c70aa0d815fa94e75d6078bbebdfe1d1cd5
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2024-02-27 17:36:55 -0300

    chamada da API
    documento JavaScript dedicada exclusivamente a executar a chamada da API

---
+++ scripts/fetch.js
@@ -0,0 +1,36 @@
+const pokemonImage = document.querySelector('#pokemonImage');
+
+function showingPokemonData(pokemonId) {
+    // URL da PokeAPI para obter informações sobre um Pokémon específico
+    url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`;
+    // realizando a requisição
+    fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            inputBtnTry.removeAttribute('disabled');
+
+            throw new Error(`Erro ${response.status}: Não foi possível obter os dados do Pokémon.`);
+        }
+
+        return response.json();
+    })
+    .then(pokemonData => {
+        const pokeData = {
+            name: pokemonData.name,
+            link: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonData.id}.png`
+        }
+
+        const newNamePokemon = new String(pokeData.name).toString().replaceAll('-', ' ');
+        const img = document.querySelector('img');
+        img.src = pokeData.link;
+        img.alt = newNamePokemon;
+
+        console.log(newNamePokemon);
+        inputBtnTry.removeAttribute('disabled');
+    })
+    .catch(error => {
+        console.error(error);
+
+        inputBtnTry.removeAttribute('disabled');
+    });
+}
